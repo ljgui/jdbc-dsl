cb989b44c290f4f22f78fea7ef16d6e81815735a
lujiangui <276221755@qq.com>
2022-08-12 16:43:12 +0800

    增加聚合函数的支持

--- src/test/java/org/jdbc/dsl/DefaultQuerySqlBuilderTest.java
+++ src/test/java/org/jdbc/dsl/DefaultQuerySqlBuilderTest.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jdbc.dsl.anotation.Column;
 import org.jdbc.dsl.core.Query;
+import org.jdbc.dsl.operator.FunctionColumn;
+import org.jdbc.dsl.operator.dml.query.SelectColumn;
 import org.jdbc.dsl.param.QueryParam;
 import org.jdbc.dsl.param.TermType;
 import org.jdbc.dsl.supports.DialectType;
@@ -130,16 +132,78 @@ public class DefaultQuerySqlBuilderTest {
 
         Assert.assertNotNull(sqlRequest);
         Assert.assertNotNull(sqlRequest.getSql());
-        System.out.println(((PrepareSqlRequest) sqlRequest).toNativeSql());
+        System.out.println((sqlRequest).toNativeSql());
 
     }
 
 
     @Test
-    public void testColumn(){
-        String[] column = PropertyUtils.getColumn(UserEntity.class);
-        Arrays.stream(column).forEach(System.out::println);
+    public void testAlias() {
+        BuildParameterQueryOperator query = new BuildParameterQueryOperator("test");
+        Query<Object, QueryParam> q = Query.of()
+                .like$("phone",  "135")
+                .includes("id","age","img")
+                .in("name",Arrays.asList("1","2","3"))
+                .doPaging(0,20);
+        query.setParam(q.getParam());
+        query.select("*")
+                .select(SelectColumn.of("age","ageAlias"))
+                .where(dsl ->
+                        dsl.is("name", "1234")
+                                .is("info.comment", "1234")
+                                .gt(UserEntity::getId,"abc")
+                )
+                .paging(0, 10);
+
+
+        DefaultQuerySqlBuilder sqlBuilder = DefaultQuerySqlBuilder.of(DialectType.POSTGRESQL);
+        SqlRequest sqlRequest = sqlBuilder.build(query.getParameter());
+        System.out.println((sqlRequest).toNativeSql());
+
     }
+
+    @Test
+    public void testFunction() {
+        BuildParameterQueryOperator query = new BuildParameterQueryOperator("test");
+        query.select(SelectColumn.of("age","total").max())
+                .where(dsl ->
+                        dsl.is("name", "1234").gte("age",20)
+                                .gt(UserEntity::getId,"abc")
+                );
+
+
+        DefaultQuerySqlBuilder sqlBuilder = DefaultQuerySqlBuilder.of(DialectType.POSTGRESQL);
+        SqlRequest sqlRequest = sqlBuilder.build(query.getParameter());
+        System.out.println((sqlRequest).toNativeSql());
+
+    }
+
+
+    @Test
+    public void testMethodRefrence(){
+        BuildParameterQueryOperator operator = new BuildParameterQueryOperator(UserEntity.class);
+        Query<Object, QueryParam> query = Query.of()
+                .and(UserEntity::getId,"6666")
+                .like$(UserEntity::getUserName,"张三")
+                .$like$(UserEntity::getDescrition,"旺财")
+                .or(UserEntity::getAccount,"wangcai")
+                .includes(UserEntity::getDeptId, UserEntity::getAge, UserEntity::getUserName);
+        //子查询
+        NestConditional<Query<Object, QueryParam>> nest = query.nest();
+        nest.and(UserEntity::getAge,TermType.gte,18)
+                .and(UserEntity::getAge,TermType.lte,25);
+
+        operator.setParam(query.getParam());
+        DefaultQuerySqlBuilder sqlBuilder = DefaultQuerySqlBuilder.of(DialectType.MYSQL);
+        SqlRequest sqlRequest = sqlBuilder.build(operator.getParameter());
+//        Assert.assertEquals(sqlRequest.getSql(),"SELECT \"DEPT_ID\",\"AGE\",\"USER_NAME\" FROM USER_ENTITY WHERE ID = ? and USER_NAME like ? and DESCRITION like ? or ACCOUNT = ? and ( AGE >= ? and AGE <= ? )");
+        Assert.assertEquals(sqlRequest.getSql(),"SELECT `DEPT_ID`,`AGE`,`USER_NAME` FROM USER_ENTITY WHERE ID = ? and USER_NAME like ? and DESCRITION like ? or ACCOUNT = ? and ( AGE >= ? and AGE <= ? )");
+        Assert.assertEquals(sqlRequest.getParameters()[0],"6666");
+        Assert.assertEquals(sqlRequest.getParameters()[1],"张三%");
+        Assert.assertEquals(sqlRequest.getParameters()[2],"%旺财%");
+        Assert.assertEquals(sqlRequest.getParameters()[3],"wangcai");
+    }
+
 
 
     @Setter
